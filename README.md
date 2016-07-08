c401d48ee1131815fee23b22d80cf27e3df56e2a
Sonaza <temu92@gmail.com>
2016-07-09 01:09:52 +0300

    Initial commit at version 1.1.0

---
+++ README.md
@@ -0,0 +1,20 @@
+# DemonMaster
+Simple helper addon for demon handling for warlocks in World of Warcraft.
+
+## Description
+Tired of fiddly flyout menus to summon your demons? Doesn't your keybinds get sometimes confusing? Introducing DemonMaster, the new favorite of every practicing demon summoner.
+
+With this addon a warlock can open up a simple dialog where they can type the name of the demon. Immediately when a match is found you can hit the enter once to confirm it is the right one and second time to summon it.
+
+The addon automatically detects if you have Grimoire of Supremacy talent selected and will even match the demon names: "Succubus" and "Shivarra" will both find the correct demon regardless of your current talent selection. Additionally only demons that you can summon are displayed.
+
+The addon /should/ work with all/most locales though it has only been tested with the English one.
+
+### Usage
+
+To begin with you must assign a key bind for the dialog. In case one has not been set the addon should prompt for it. If not, go to the Key Bindings menu from the Game Menu (hint: press ESC) and then browse to AddOns category. From there you will find the option to bind a key.
+
+After you are all set with the key binding you can hit it whenever you like provided you are only out of combat and start summoning!
+
+## Dependencies
+DemonMaster uses Ace3 which is included in the /libs directory.
---
+++ core.lua
@@ -0,0 +1,194 @@
+local addon_name, addon_shared = ...;
+
+local _G = getfenv(0);
+
+local LibStub = LibStub;
+local Addon = LibStub("AceAddon-3.0"):NewAddon(addon_name, "AceEvent-3.0");
+_G[addon_name] = Addon;
+
+_G["BINDING_HEADER_DEMONMASTER"] = "DemonMaster";
+_G["BINDING_NAME_TOGGLE_DEMONMASTER"] = "Toggle DemonMaster";
+
+StaticPopupDialogs["DEMONMASTER_NO_KEYBIND"] = {
+	text = "DemonMaster does not currently have a keybinding. Do you want to open the key binding menu to set it?|n|nOption you are looking for is found under AddOns category.",
+	button1 = YES,
+	button2 = NO,
+	button3 = "Don't Ask Again",
+	OnAccept = function(self)
+		PlaySound("igMainMenuOption");
+		KeyBindingFrame_LoadUI();
+		KeyBindingFrame.mode = 1;
+		ShowUIPanel(KeyBindingFrame);
+	end,
+	OnCancel = function(self)
+	end,
+	OnAlt = function()
+		DemonMasterKeybindAlert = true;
+	end,
+	hideOnEscape = 1,
+	timeout = 0,
+}
+
+local DemonSpells = {
+	688,	-- Imp
+	697,	-- Voidwalker
+	712,	-- Succubus
+	691,	-- Felhunter
+	30146, 	-- Felguard
+	1122,	-- Infernal
+	18540,	-- Doomguard
+};
+
+local MESSAGE_PATTERN = "|cffe8608fDemonMaster|r %s";
+function Addon:AddMessage(pattern, ...)
+	DEFAULT_CHAT_FRAME:AddMessage(MESSAGE_PATTERN:format(string.format(pattern, ...)));
+end
+
+function Addon:OnInitialize()
+	
+end
+
+local _, PLAYER_CLASS = UnitClass("player");
+
+function Addon:IsBindingSet()
+	return GetBindingKey("TOGGLE_DEMONMASTER") ~= nil;
+end
+
+function Addon:OnEnable()
+	if(PLAYER_CLASS == "WARLOCK") then
+		Addon:RegisterEvent("PLAYER_REGEN_DISABLED");
+		
+		if(not Addon:IsBindingSet() and not DemonMasterKeybindAlert) then
+			StaticPopup_Show("DEMONMASTER_NO_KEYBIND");
+		end
+	end
+end
+
+function Addon:GetRealSpellID(spell_id)
+	local spell_name = GetSpellInfo(spell_id);
+	local name, _, _, _, _, _, realSpellID = GetSpellInfo(spell_name);
+	
+	return realSpellID or spell_id;
+end
+
+function Addon:PLAYER_REGEN_DISABLED()
+	Addon:CloseFrame();
+end
+
+function Addon:ResetFrame()
+	DemonMasterFrameSearch:SetText("");
+end
+
+function Addon:ToggleFrame()
+	if(InCombatLockdown()) then return end
+	if(PLAYER_CLASS ~= "WARLOCK") then Addon:AddMessage("Not a warlock"); return end
+	
+	if(not DemonMasterFrame:IsShown()) then
+		Addon:OpenFrame();
+	else
+		Addon:CloseFrame();
+	end
+end
+
+function Addon:OpenFrame()
+	if(PLAYER_CLASS ~= "WARLOCK") then return end
+	
+	if(DemonMasterFrame:IsShown()) then return end
+	
+	-- if(not Addon.CurrentBinding) then
+		-- Addon.CurrentBinding = GetBindingAction("ENTER");
+		-- SetBinding("ENTER", "CLICK DemonMasterFrameSpellButton:LeftButton");
+		-- SetOverrideBindingClick(DemonMasterFrameSearch, true, "ENTER", "DemonMasterFrameSpellButton", "LeftButton");
+	-- end
+	
+	Addon:ResetFrame();
+	DemonMasterFrame:Show();
+	
+	DemonMasterFrameSearch:Show();
+	DemonMasterFrameSearch:SetFocus();
+	
+	DemonMasterFrameSpellConfirm:Hide();
+end
+
+function Addon:CloseFrame()
+	if(Addon.CurrentBinding) then
+		SetBinding("ENTER", Addon.CurrentBinding);
+		Addon.CurrentBinding = nil;
+	end
+	
+	DemonMasterFrame:Hide();
+end
+
+function DemonMaster_OnEditFocusLost()
+	-- Addon:CloseFrame()
+end
+
+function DemonMaster_OnEnterPressed(self)
+	if(not Addon.CurrentBinding) then
+		Addon.CurrentBinding = GetBindingAction("ENTER");
+		SetBinding("ENTER", "CLICK DemonMasterFrameSpellButton:LeftButton");
+	end
+	
+	DemonMasterFrameSearch:Hide();
+	DemonMasterFrameSpellConfirm:Show();
+end
+
+function DemonMaster_OnTextChanged(self)
+	SearchBoxTemplate_OnTextChanged(self)
+	
+	local searchText = strtrim(strlower(self:GetText()));
+	
+	if(strlen(searchText) > 0) then
+		local tokens = { strsplit(" ", searchText) };
+		
+		for index, spellID in ipairs(DemonSpells) do
+			if(IsSpellKnown(spellID)) then
+				local spellName = GetSpellInfo(spellID);
+				
+				local realSpellID = Addon:GetRealSpellID(spellID);
+				local realSpellName, _, realIcon = GetSpellInfo(realSpellID);
+				
+				-- local searchSpellName = gsub(spellName, "Teleport: ", "");
+				local searchSpellName = realSpellName;
+				
+				local spellFound = true;
+				for _, token in ipairs(tokens) do
+					spellFound = spellFound and (strmatch(strlower(spellName), token) or strmatch(strlower(searchSpellName), token));
+				end
+				
+				if(spellFound) then
+					DemonMasterFrameSpellName:SetText(realSpellName);
+					
+					DemonMasterFrameSpellButton.icon:SetTexture(realIcon);
+					DemonMasterFrameSpellButton.iconBorder:SetVertexColor(0.1, 0.1, 0.1);
+					
+					DemonMasterFrameSpellButton:SetAttribute("type", "spell");
+					DemonMasterFrameSpellButton:SetAttribute("spell", realSpellName);
+					
+					DemonMasterFrameSpellButton:Show();
+					
+					break;
+				else
+					DemonMasterFrameSpellName:SetText("No Result");
+					DemonMasterFrameSpellButton:Hide();
+				end
+			end
+		end
+	else
+		DemonMasterFrameSpellName:SetText("Enter Demon Name");
+		DemonMasterFrameSpellButton:Hide();
+	end
+end
+
+function DemonMaster_OnEscapePressed(self)
+	self:ClearFocus();
+	Addon:CloseFrame();
+end
+
+function DemonMaster_CloseFrame()
+	Addon:CloseFrame();
+end
+
+function Addon:OnDisable()
+		
+end
